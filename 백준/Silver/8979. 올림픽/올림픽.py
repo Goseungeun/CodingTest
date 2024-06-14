f45ca539323ddbf541d99167bb1ae35bd9671687
Goseungeun <74888933+Goseungeun@users.noreply.github.com>
2024-06-14 16:55:33 +0900

    [Silver V] Title: 올림픽, Time: 84 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -3,19 +3,8 @@ countries = [list(map(int,input().split())) for _ in range(n)]
 
 countries.sort(key=lambda x: (x[1],x[2],x[3]),reverse= True)
 #print(countries)
-rank = 1
-prev_g,prev_s,prev_b = countries[0][1],countries[0][2],countries[0][3]
-i = 0
-
-while countries[i][0] != k:
-    if prev_g != countries[i][1] or prev_s != countries[i][2] or prev_b != countries[i][3]:
-        #print("here")
-        rank = i+1
-    prev_g,prev_s,prev_b = countries[i][1],countries[i][2],countries[i][3]
-    
-    i += 1
-
-if prev_g != countries[i][1] or prev_s != countries[i][2] or prev_b != countries[i][3]:
-    rank += 1
-
-print(rank)
+idx = [countries[i][0] for i in range(n)].index(k)
+for i in range(n):
+    if countries[idx][1:] == countries[i][1:]:
+        print(i+1)
+        break
